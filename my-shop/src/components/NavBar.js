12aa98a1e259f7432a42f6626bddb0217aee3733
agustin mateos <agustinrotondomateos@gmail.com>
2022-11-18 01:01:25 -0300

    shadow footer navbar

--- my-shop/src/components/NavBar.js
+++ my-shop/src/components/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = ()=>{
 
     const {count}=useCart()
 
-    return(<div className="bg-white shadow-sm mb-3 NavBarContainer">
+    return(<div className="shadow-lg p-3 mb-5 bg-white rounded NavBarContainer">
        <Link to='/'>Home</Link>
        <Link to='/cart'><button><img style={{whidth:"3rem", height:"2rem"}} src="../imagenes/cart.png"></img>{count}</button></Link>
         </div>)
